aeb02855127473a417212321b9155defed448353
Miguel Mota <miguelmota2@gmail.com>
2015-03-10 23:17:35 -0700

    NaN test

--- test/is-falsy.js
+++ test/is-falsy.js
@@ -2,12 +2,13 @@ var test = require('tape');
 var isFalsy = require('../is-falsy');
 
 test('isFalsy', function (t) {
-  t.plan(8);
+  t.plan(9);
 
   t.true(isFalsy(''));
   t.true(isFalsy(0));
   t.true(isFalsy(null));
   t.true(isFalsy(undefined));
+  t.true(isFalsy(NaN));
   t.false(isFalsy(1));
   t.false(isFalsy({}));
   t.false(isFalsy([]));
